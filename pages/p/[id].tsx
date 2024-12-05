487cebf15435af9002c5021465005ba80a395612
Jake Hayes <jhayes@tcetra.com>
2024-12-05 14:55:28 -0500

    I don't even know why I'm doing this

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -3,35 +3,25 @@ import { GetServerSideProps } from "next"
 import ReactMarkdown from "react-markdown"
 import Layout from "../../components/Layout"
 import { PostProps } from "../../components/Post"
+import prisma from "../../lib/prisma"
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = {
-    id: "1",
-    title: "Prisma is the perfect ORM for Next.js",
-    content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-    published: false,
-    author: {
-      name: "Nikolas Burk",
-      email: "burk@prisma.io",
-    },
-  }
+  const person = prisma.reviewer.findFirst({
+    where: { id: Number(params?.id), }
+  })
   return {
-    props: post,
+    props: person,
   }
 }
 
 const Post: React.FC<PostProps> = (props) => {
-  let title = props.title
-  if (!props.published) {
-    title = `${title} (Draft)`
-  }
+  let title = props.name
 
   return (
     <Layout>
       <div>
         <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
-        <ReactMarkdown children={props.content} />
+        <p>By {props?.name || "Unknown reviewer"}</p>
       </div>
       <style jsx>{`
         .page {
